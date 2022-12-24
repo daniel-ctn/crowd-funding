c51a0aa03e23e537d92f4fea297792f09a502422
Daniel Nguyen <danielnguyen5201@gmail.com>
2022-12-25 00:20:11 +0700

    add tailwind to project

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,22 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
+import { BrowserRouter } from 'react-router-dom'
+
+import './index.css'
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
+const activeChainId = ChainId.Goerli
 
-const container = document.getElementById("root");
-const root = createRoot(container!);
+const container = document.getElementById('root')
+const root = createRoot(container!)
 root.render(
   <React.StrictMode>
     <ThirdwebProvider desiredChainId={activeChainId}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThirdwebProvider>
   </React.StrictMode>
-);
+)
